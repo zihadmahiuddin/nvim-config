1dab5946a2f2a10481fd69d54314741b80bec249
Zihad <zihadmahiuddin@gmail.com>
2023-02-27 14:13:16 +0600

    plugins: add wakatime

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -2,6 +2,8 @@ return {
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
 
+  'wakatime/vim-wakatime',
+
   -- Useful plugin to show you pending keybinds.
   { 'folke/which-key.nvim',          opts = {} },
 }
